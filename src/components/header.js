0d21b7648645dd269b51dc608edbed7198e57732
Felipe Argôlo <felipeargolo75@gmail.com>
2025-03-18 13:20:01 -0300

    header incompleto (mas quase completo)

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,19 @@ export function createHeader() {
     header.classList.add("header");
 
     header.innerHTML = `
-        <nav>
-            <a href="./index.html">Home</a>
-            <a href="#">Sobre</a>
-            <a href="./offers.html">Ofertas</a>
-            <a href="#">Contato</a>
+        <nav class="header">
+
+        <div class="logo">
+            <img src="../assets/img/Logo.png" alt="Logo" />
+        </div>
+
+        <div class="clickables">
+            <a href="./index.html">HOME</a>
+            <a href="#">SOBRE</a>
+            <a href="./offers.html">OFERTAS ESPECIAIS</a>
+            <a href="#">CONTATO</a>
+        </div>
+
         </nav>
     `;
 
