2d685d7d852b2c92d13ac84cc1186532b433621b
Felipe Argôlo <felipeargolo75@gmail.com>
2025-03-17 22:35:03 -0300

    HomePage completa

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -5,14 +5,18 @@ export function createHomePage() {
 
     main.innerHTML = `
         <section class="intro">
-            <h1>Bem-vindo à nossa página</h1>
-            <p>Explore os nossos serviços e entre em contato conosco!</p>
+        
+        <div class="title_homePage">
+            <h1>Sua Estadia dos Sonhos <p id="special_h1"> Começa aqui </p></h1>
+            <hr width= "83%" color="white">
+            <p id="description">Hospedagem com excelência, para momentos que ficam na memória.</p>
+        </div>
+        
+
         </section>
 
         <section class="imagens">
-            <img src="https://source.unsplash.com/600x400/?nature" alt="Natureza">
-            <img src="https://source.unsplash.com/600x400/?city" alt="Cidade">
-            <img src="https://source.unsplash.com/600x400/?technology" alt="Tecnologia">
+           
         </section>
     `;
 
